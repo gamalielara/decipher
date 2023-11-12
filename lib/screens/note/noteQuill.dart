0d9d972d3e849edffd853536e884a1f1002816c4
gamalielara <aragamalielboanerges@gmail.com>
2023-11-12 18:50:57 +0700

    get the quill value

--- lib/screens/note/noteQuill.dart
+++ lib/screens/note/noteQuill.dart
@@ -1,70 +1,88 @@
 import 'package:decipher/screens/note/noteQuillToolbar.dart';
-import 'package:decipher/theme/colors.dart';
 import 'package:decipher/theme/text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 
-class NoteQuill extends StatelessWidget {
+class NoteQuill extends StatefulWidget {
   const NoteQuill({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final QuillController _controller = QuillController.basic();
+  State<NoteQuill> createState() => _NoteQuillState();
+}
 
+class _NoteQuillState extends State<NoteQuill> {
+  final QuillController _controller = QuillController.basic();
+  Delta noteValue = Delta.fromJson([]);
+
+  @override
+  Widget build(BuildContext context) {
+    _controller.addListener(() {
+      print("EDITOR VALUE TO JSON ${_controller.document.toDelta().toJson()}");
+      // setState(() {
+      //   noteValue =
+      //   // print("EDITOR VALUE: ${noteValue}");
+      // });
+    });
     return QuillProvider(
-        configurations: QuillConfigurations(
-          controller: _controller,
-          sharedConfigurations: QuillSharedConfigurations(
-            locale: Locale('en'),
-          ),
+      configurations: QuillConfigurations(
+        controller: _controller,
+        sharedConfigurations: const QuillSharedConfigurations(
+          locale: Locale('en'),
         ),
-        child: Container(
-            child: Expanded(
+      ),
+      child: Container(
+        child: Expanded(
           child: Column(
             children: [
               NoteQuillToolbar(controller: _controller),
-              QuillEditor.basic(
-                configurations: QuillEditorConfigurations(
-                  readOnly: false,
-                  scrollable: true,
-                  customStyles: DefaultStyles(
-                    bold: boldText,
-                    paragraph: DefaultTextBlockStyle(
-                        regularText.copyWith(fontSize: 16),
-                        const VerticalSpacing(1, 1),
-                        const VerticalSpacing(1, 1),
-                        const BoxDecoration()),
-                    h1: DefaultTextBlockStyle(
-                        boldText.copyWith(fontSize: 25),
-                        const VerticalSpacing(15, 15),
-                        const VerticalSpacing(15, 15),
-                        const BoxDecoration()),
-                    h2: DefaultTextBlockStyle(
-                        boldText.copyWith(fontSize: 20),
-                        const VerticalSpacing(15, 15),
-                        const VerticalSpacing(15, 15),
-                        const BoxDecoration()),
-                    h3: DefaultTextBlockStyle(
-                        boldText.copyWith(fontSize: 15),
-                        const VerticalSpacing(15, 15),
-                        const VerticalSpacing(15, 15),
-                        const BoxDecoration()),
-                    lists: DefaultListBlockStyle(
-                        regularText.copyWith(fontSize: 16),
-                        const VerticalSpacing(1, 1),
-                        const VerticalSpacing(1, 1),
-                        const BoxDecoration(), null),
-                    leading: DefaultTextBlockStyle(
-                        regularText.copyWith(fontSize: 16),
-                        const VerticalSpacing(1, 1),
-                        const VerticalSpacing(1, 1),
-                        const BoxDecoration()),
-                    color: const Color(0xFFE5E5E5),
+              Flexible(
+                child: QuillEditor.basic(
+                  configurations: QuillEditorConfigurations(
+                    readOnly: false,
+                    scrollable: true,
+                    minHeight: double.infinity,
+                    customStyles: DefaultStyles(
+                      bold: boldText,
+                      paragraph: DefaultTextBlockStyle(
+                          regularText.copyWith(fontSize: 16),
+                          const VerticalSpacing(1, 1),
+                          const VerticalSpacing(1, 1),
+                          const BoxDecoration()),
+                      h1: DefaultTextBlockStyle(
+                          boldText.copyWith(fontSize: 25),
+                          const VerticalSpacing(15, 15),
+                          const VerticalSpacing(15, 15),
+                          const BoxDecoration()),
+                      h2: DefaultTextBlockStyle(
+                          boldText.copyWith(fontSize: 20),
+                          const VerticalSpacing(15, 15),
+                          const VerticalSpacing(15, 15),
+                          const BoxDecoration()),
+                      h3: DefaultTextBlockStyle(
+                          boldText.copyWith(fontSize: 15),
+                          const VerticalSpacing(15, 15),
+                          const VerticalSpacing(15, 15),
+                          const BoxDecoration()),
+                      lists: DefaultListBlockStyle(
+                          regularText.copyWith(fontSize: 16),
+                          const VerticalSpacing(1, 1),
+                          const VerticalSpacing(1, 1),
+                          const BoxDecoration(),
+                          null),
+                      leading: DefaultTextBlockStyle(
+                          regularText.copyWith(fontSize: 16),
+                          const VerticalSpacing(1, 1),
+                          const VerticalSpacing(1, 1),
+                          const BoxDecoration()),
+                      color: const Color(0xFFE5E5E5),
+                    ),
                   ),
                 ),
               ),
             ],
           ),
-        )));
+        ),
+      ),
+    );
   }
 }
